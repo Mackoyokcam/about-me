0523598fa7e5941f0ebd4f7be8bb464db461b480
mackoyokcam <mark.hangarter@gmail.com>
2017-07-11 14:09:47 -0700

    Switched arrays into one object, added console logs for debugging

--- app.js
+++ app.js
@@ -6,40 +6,53 @@ var userAnswer;
 var rightAnswerCount = 0;
 
 // Yes or no questions for guessing game
-var questions = [
-  "Is my favorite color blue?",
-  "Do I like to play video games?",
-  "Do I have 2 dogs as pets?",
-  "Do I have a cat as a pet?",
-  "Is 'Troy' the best movie out there?"
-];
+var questionsAndAnswers = {
+  "Is my favorite color blue?": {
+    "answer": "yes",
+    "correctResponse": "Blue is my favorite color!",
+    "wrongResponse": "Wrong! I love blue..."
+  },
+  "Do I like to play video games?": {
+    "answer": "yes",
+    "correctResponse": "Yep! Every video game there is!",
+    "wrongResponse": "Nope! Why would you think that?!"
+  },
+  "Do I have 2 dogs as pets?": {
+    "answer": "no",
+    "correctResponse": "Correct! I have three dogs.",
+    "wrongResponse": "Trick question! I have three dogs!"
+  },
+  "Do I have a cat as a pet?": {
+    "answer": "no",
+    "correctResponse": "Correct! Cat's are evil!",
+    "wrongResponse": "Wrong! Cat's are evil!"
+  },
+  "Is 'Troy' the best movie out there?": {
+    "answer": "yes",
+    "correctResponse": "Of course it is.",
+    "wrongResponse": "Have you even seen it?!? I demand you watch it right now."
+  }
+};
 
-// Answers to guessing game (Match indices with questions array!)
-var answers = [
-  "yes",
-  "yes",
-  "no",
-  "no",
-  "yes",
-];
-
-// Questions and Responses
+// Get user name
 userName = prompt(getUserName);
+console.log("The user's name is " + userName);
 alert("Ok " +  userName + ", let's see if you can guess 5 things about me. Ready?");
 
-for (var i = 0; i < questions.length; i++) {
-  userAnswer = prompt(questions[i]);
+// Cycle through questions and responses, get user answers
+for (var key in questionsAndAnswers) {
 
-  console.log("User's answer = " + userAnswer.toLowerCase() + ", Actual answer = " + answers[i]);
+  // Ask the question
+  var currentQuestion = questionsAndAnswers[key];
+  userAnswer = prompt(key);
+  console.log("User's answer = " + userAnswer.toLowerCase() + ", Actual answer = " + currentQuestion["answer"]);
 
-  if (userAnswer.toLowerCase() === answers[i]) {
-    alert("You got it right!");
+  // Evaluate the response
+  if (userAnswer.toLowerCase() === currentQuestion["answer"]) { //correct answer
+    alert(currentQuestion["correctResponse"]);
     rightAnswerCount++;
-  } else {
-    alert("You got it wrong :(");
+    console.log("User has answered " + rightAnswerCount + " questions correctly.");
+  } else { // wrong answer
+    alert(currentQuestion["wrongResponse"]);
   }
 }
-
-// Console logs
-console.log("The user's name is " + userName);
-console.log("The user answered " + rightAnswerCount + " questions correctly");
