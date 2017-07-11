bc1ad903a96eba229db1d5b18f4a6764dc08fa93
mackoyokcam <mark.hangarter@gmail.com>
2017-07-11 15:26:56 -0700

    added css and html tags and content. Included a profile picture

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-"use strict";
+'use strict';
 // Variables for acquiring the name of the user
 var userName;
-var getUserName = "What is your name?";
+var getUserName = 'What is your name?';
 var userAnswer;
 var rightAnswerCount = 0;
 
 // Yes or no questions for guessing game
 var questionsAndAnswers = {
-  "Is my favorite color blue?": {
-    "answer": "yes",
-    "correctResponse": "Blue is my favorite color!",
-    "wrongResponse": "Wrong! I love blue..."
+  'Is my favorite color blue?': {
+    'answer': 'yes',
+    'correctResponse': 'Blue is my favorite color!',
+    'wrongResponse': 'Wrong! I love blue...'
   },
-  "Do I like to play video games?": {
-    "answer": "yes",
-    "correctResponse": "Yep! Every video game there is!",
-    "wrongResponse": "Nope! Why would you think that?!"
+  'Do I like to play video games?': {
+    'answer': 'yes',
+    'correctResponse': 'Yep! Every video game there is!',
+    'wrongResponse': 'Nope! Why would you think that?!'
   },
-  "Do I have 2 dogs as pets?": {
-    "answer": "no",
-    "correctResponse": "Correct! I have three dogs.",
-    "wrongResponse": "Trick question! I have three dogs!"
+  'Do I have 2 dogs as pets?': {
+    'answer': 'no',
+    'correctResponse': 'Correct! I have three dogs.',
+    'wrongResponse': 'Trick question! I have three dogs!'
   },
-  "Do I have a cat as a pet?": {
-    "answer": "no",
-    "correctResponse": "Correct! Cat's are evil!",
-    "wrongResponse": "Wrong! Cat's are evil!"
+  'Do I have a cat as a pet?': {
+    'answer': 'no',
+    'correctResponse': 'Correct! Cat\'s are evil!',
+    'wrongResponse': 'Wrong! Cat\'s are evil!'
   },
-  "Is 'Troy' the best movie out there?": {
-    "answer": "yes",
-    "correctResponse": "Of course it is.",
-    "wrongResponse": "Have you even seen it?!? I demand you watch it right now."
+  'Is "Troy" the best movie out there?': {
+    'answer': 'yes',
+    'correctResponse': 'Of course it is.',
+    'wrongResponse': 'Have you even seen it?!? I demand you watch it right now.'
   }
 };
 
 // Get user name
 userName = prompt(getUserName);
-console.log("The user's name is " + userName);
-alert("Ok " +  userName + ", let's see if you can guess 5 things about me. Ready?");
+console.log('The user\'s name is ' + userName);
+alert('Ok ' + userName + ', let\'s see if you can guess 5 things about me. Ready?');
 
 // Cycle through questions and responses, get user answers
 for (var key in questionsAndAnswers) {
@@ -45,14 +45,14 @@ for (var key in questionsAndAnswers) {
   // Ask the question
   var currentQuestion = questionsAndAnswers[key];
   userAnswer = prompt(key);
-  console.log("User's answer = " + userAnswer.toLowerCase() + ", Actual answer = " + currentQuestion["answer"]);
+  console.log('User\'s answer = ' + userAnswer.toLowerCase() + ', Actual answer = ' + currentQuestion['answer']);
 
   // Evaluate the response
-  if (userAnswer.toLowerCase() === currentQuestion["answer"]) { //correct answer
-    alert(currentQuestion["correctResponse"]);
+  if (userAnswer.toLowerCase() === currentQuestion['answer']) { //correct answer
+    alert(currentQuestion['correctResponse']);
     rightAnswerCount++;
-    console.log("User has answered " + rightAnswerCount + " questions correctly.");
+    console.log('User has answered ' + rightAnswerCount + ' questions correctly.');
   } else { // wrong answer
-    alert(currentQuestion["wrongResponse"]);
+    alert(currentQuestion['wrongResponse']);
   }
 }
