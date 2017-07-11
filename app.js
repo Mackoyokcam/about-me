eec946c54f46a9b7b235cfa351e239e6ecd4fd67
mackoyokcam <mark.hangarter@gmail.com>
2017-07-11 13:13:53 -0700

    added empty arrays for questions and answers. Did a test for push function on array for functionality

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 "use strict";
+// Variables for acquiring the name of the user
+var userName;
+var getUserName = "What is your name?";
+var userAnswers = [];
+
+// Yes or no questions for guessing game
+var questions = [
+  "",
+  "",
+  "",
+  "",
+  ""
+];
+
+// Answers to guessing game (Match indices with questions array!)
+var answers = [
+  "",
+  "",
+  "",
+  "",
+  "",
+];
+
+// Questions and Responses
+userAnswers.push("hi");
+alert(userAnswers[0]);
+
+// Console logs
